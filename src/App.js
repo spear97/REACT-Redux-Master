10803e379ff7227e68d430017ad35eb0ccac8c77
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 14:03:25 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainPanel from './components/MainPanel';
 
+// Define the main App component
 function App() {
   return (
-      <div>
-        <MainPanel/>
-      </div>
-    );
+    <div>
+      {/* Render the MainPanel component */}
+      <MainPanel/>
+    </div>
+  );
 }
 
 export default App;
