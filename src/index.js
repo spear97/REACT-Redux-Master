59d4ed8170e8894b421e036a80878c1331d1c752
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 14:02:20 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
-import {Provider} from 'react-redux'
-import myReducers from './reducers'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import myReducers from './reducers';
 
-
-//Create the store
+// Create the store
 const myStore = createStore(myReducers);
 
-//This will console log the current state everytime the state changes
-myStore.subscribe(()=>console.log(myStore.getState()));
+// This will console log the current state every time the state changes
+myStore.subscribe(() => console.log(myStore.getState()));
 
-//Enveloping the App inside the Provider, ensures that the states in the store are available
-//throughout the application
-ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
+// Enveloping the App inside the Provider ensures that the states in the store are available
+// throughout the application
+ReactDOM.render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
