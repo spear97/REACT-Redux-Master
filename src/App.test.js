d427546c024cab341e3000478522e525404bc398
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-20 14:02:44 -0600

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// Import necessary testing utilities from '@testing-library/react'
 import { render, screen } from '@testing-library/react';
+
+// Import the main App component to be tested
 import App from './App';
 
+// Define a test case to check if the 'learn react' link is rendered
 test('renders learn react link', () => {
+  // Render the App component
   render(<App />);
+  
+  // Use 'screen.getByText' to find the element containing 'learn react' text
   const linkElement = screen.getByText(/learn react/i);
+  
+  // Assert that the 'learn react' link is present in the document
   expect(linkElement).toBeInTheDocument();
 });
